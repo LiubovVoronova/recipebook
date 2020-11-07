3270b1c8eae3b678902d66ea9e5b78805b1083ab
Lyubov Voronova <lyubov.voronova@quest.com>
2020-11-07 11:47:02 +0300

    added more recipes to main page

--- src/app/recipes/recipe-item.component.ts
+++ src/app/recipes/recipe-item.component.ts
@@ -4,12 +4,23 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
   selector: 'app-recipe-item',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{recipeItem.title}}</h2>
-      <div>{{recipeItem.description}}</div>
+      <img src="{{recipeItem.image}}" alt="recipeItem.title">
+      <div>
+        <h3>{{recipeItem.title}}</h3>
+        <div>{{recipeItem.category}}</div>
+      </div>
     </div>`,
   styles: [`
     .well div {
       color: #bbb;
+    }
+    img {
+      width: 150px;
+      float: left;
+      margin-right: 20px;
+    }
+    .thumbnail {
+      min-height: 190px;
     }
   `]
 })
