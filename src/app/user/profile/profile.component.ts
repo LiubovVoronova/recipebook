ba779c05b196f4f7325283016d084dd72e3881b6
LubovVoronova <lubov.voronova88@gmail.ru>
2020-11-20 16:20:49 +0300

    reactive-form for profile page

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent  {
+export class ProfileComponent  implements OnInit {
+  profileForm: FormGroup;
+  private firstName;
+  private lastName;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
+  ngOnInit(): void {
+    this.firstName = new FormControl(this.authService.currentUser.firstName,
+      [Validators.required, Validators.pattern('[a-zA-z].*')]);
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
+    this.profileForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName
+    })
+  }
 
+  validateFirstName() {
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.untouched;
+  }
+
+  onSaveProfile(formValues) {
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['recipes'])
+    }
+  }
+
+  onCancel() {
+    this.router.navigate(['recipes'])
+  }
 
 }
