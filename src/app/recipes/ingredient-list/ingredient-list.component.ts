07728bb6d5ce4cb446bdc98c808c45eb9251e8dd
LubovVoronova <lubov.voronova88@gmail.ru>
2020-11-23 15:44:59 +0300

    added ingredient-list

---
+++ src/app/recipes/ingredient-list/ingredient-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Ingredient } from "../shared/recipe.model";
+
+@Component({
+  selector: 'app-ingredient-list',
+  templateUrl: './ingredient-list.component.html',
+  styleUrls: ['./ingredient-list.component.css']
+})
+export class IngredientListComponent implements OnInit {
+
+  @Input() ingredients: Ingredient[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
